94213ce2b8bf0c609f222d3dcd2acae9ba2ecd1e
Yurii Fito <fito.pn@ucu.edu.ua>
2026-05-02 15:37:55 +0300

    fix in rover_control: in setup file add node

--- src/indomitus_rover_control/indomitus_rover_control/rover_kinematics_node.py
+++ src/indomitus_rover_control/indomitus_rover_control/rover_kinematics_node.py
@@ -203,5 +203,5 @@ def main(args=None):
         rclpy.shutdown()
 
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
--- src/indomitus_rover_control/setup.py
+++ src/indomitus_rover_control/setup.py
@@ -24,6 +24,7 @@ setup(
     },
     entry_points={
         'console_scripts': [
+            'rover_kinematics_node = indomitus_rover_control.rover_kinematics_node:main',
         ],
     },
 )
