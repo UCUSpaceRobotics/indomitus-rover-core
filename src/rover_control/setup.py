4b4defbf7a201e7c04c08c6d97f5bd1f63c2819f
Yurii Fito <fito.pn@ucu.edu.ua>
2026-06-11 19:03:46 +0300

    fix: change description of rover_control, add dependencies

--- src/rover_control/setup.py
+++ src/rover_control/setup.py
@@ -15,8 +15,11 @@ setup(
     zip_safe=True,
     maintainer='yurifi',
     maintainer_email='fito.pn@ucu.edu.ua',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='''
+        Core control package for the Indomitus rover. Contains nodes for 
+        joystick command interpretation, kinematics calculation, 
+        and wheel-odometry estimation.''',
+    license='MIT',
     extras_require={
         'test': [
             'pytest',
