fda71f8fd26cf8ebb4963d56d5dd10c45fdc04b7
BohdanZasymovych <bohdan.zasymovych@gmail.com>
2026-06-28 15:13:46 +0300

    fix: launch file and config structure fixed to be consistent with the arguments of the launch file provided in the zed_wrapper package

--- src/rover_sensors/launch/zed2i.launch.py
+++ src/rover_sensors/launch/zed2i.launch.py
@@ -5,8 +5,7 @@ from launch_ros.substitutions import FindPackageShare
 from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
 from launch.actions import DeclareLaunchArgument
 
-DEFAULT_COMMON_STEREO_CONFIG_NAME = "common_stereo.yaml"
-DEFAULT_CAMERA_CONFIG_NAME = "zed2i.yaml"
+DEFAULT_ZED2I_CONFIG_NAME = "zed2i.yaml"
 CAMERA_MODEL = "zed2i"
 
 
@@ -14,37 +13,25 @@ def generate_launch_description():
     child_launch_file_path = PathJoinSubstitution([
         FindPackageShare("zed_wrapper"), "launch", "zed_camera.launch.py"
     ])
-    config_common_path = PathJoinSubstitution([
-        FindPackageShare("rover_sensors"), "config", DEFAULT_COMMON_STEREO_CONFIG_NAME
-    ])
-    config_camera_path = PathJoinSubstitution([
-        FindPackageShare("rover_sensors"), "config", DEFAULT_CAMERA_CONFIG_NAME
+    config_path = PathJoinSubstitution([
+        FindPackageShare("rover_sensors"), "config", DEFAULT_ZED2I_CONFIG_NAME
     ])
 
-
-    config_common_path_argument = DeclareLaunchArgument(
-        name="config_common_path",
-        default_value=config_common_path,
-        description="Path to the common stereo config for ZED stereocamera"
-    )
-    config_camera_path_argument = DeclareLaunchArgument(
-        name="config_camera_path",
-        default_value=config_camera_path,
+    config_path_argument = DeclareLaunchArgument(
+        name="config_path",
+        default_value=config_path,
         description="Path to the config for the ZED 2i stereo camera"
     )
-
 
     launch_file = IncludeLaunchDescription(
         launch_description_source=PythonLaunchDescriptionSource(child_launch_file_path),
         launch_arguments={
             "camera_model": CAMERA_MODEL,
-            "config_common_path": LaunchConfiguration("config_common_path"),
-            "config_camera_path": LaunchConfiguration("config_camera_path"),
+            "ros_params_override_path": LaunchConfiguration("config_path"),
         }.items()
     )
 
     return LaunchDescription([
-        config_common_path_argument,
-        config_camera_path_argument,
+        config_path_argument,
         launch_file,
     ])
