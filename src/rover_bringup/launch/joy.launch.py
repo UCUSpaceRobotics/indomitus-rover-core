75877226b36c62718b71bf28ed44ea2096d7c1d7
Yurii Fito <fito.pn@ucu.edu.ua>
2026-07-04 18:34:50 +0300

    Refactor/suspension/joystick update (#485)
    * refactor: make udev rule work fow any can device
    
    * refact: make toggle separate class
    
    * refact: move call logic out of joystick node
    
    * fix: change HIST_CONFIG_DIR in setup_host.sh to system
    
    * docs: ros -> indomitus-rover
    
    * bugfix: add ids filter for socket_can_receiver_node
    
    * docs: add can adress space and which adresses we already use
    
    * fix: pply review comments
    
    * fix: add game_controller_node that we used previously; also add /run/udev as volume in docker
    
    * chore: remove joy_linux from dependecies
    
    * chore: rewrite comment in english
    
    * refactor: make _publish_cmd func
    
    * refactor: remove cmd_vel_teleop node and implement this logic in joystick intepreter node
    
    * add parameter to joy.yaml file
    
    * feature: add activation button that allows/disallows sending messages on cmd_vel topic from joystick
    
    * fix: remove and add parameters from joy.launch.py
    
    * fix: fix logs in swerve_controller

--- src/rover_bringup/launch/joy.launch.py
+++ src/rover_bringup/launch/joy.launch.py
@@ -6,9 +6,8 @@ from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
-    device_path = LaunchConfiguration('device_path')
+    joy_dev = LaunchConfiguration('joy_dev')
     deadzone = LaunchConfiguration('deadzone')
-    cmd_vel_topic = LaunchConfiguration('cmd_vel_topic')
     autorepeat_rate = LaunchConfiguration('autorepeat_rate')
 
     default_config = PathJoinSubstitution([
@@ -18,28 +17,17 @@ def generate_launch_description():
     ])
 
     joy_node = Node(
-        package='joy_linux',
-        executable='joy_linux_node',
+        package='joy',
+        executable='game_controller_node',
         name='joy_node',
         output='screen',
         parameters=[{
-            'device_path': device_path,
+            'dev': joy_dev,
             'deadzone': deadzone,
             'autorepeat_rate': autorepeat_rate,
         }],
         respawn=True,
         respawn_delay=10.0
-    )
-
-    teleop_node = Node(
-        package='teleop_twist_joy',
-        executable='teleop_node',
-        name='teleop_node',
-        output='screen',
-        parameters=[default_config],
-        remappings=[
-            ('/cmd_vel', cmd_vel_topic),
-        ]
     )
 
     joy_interpreter = Node(
@@ -54,19 +42,14 @@ def generate_launch_description():
 
     return LaunchDescription([
         DeclareLaunchArgument(
-            'device_path',
+            'joy_dev',
             default_value='/dev/input/js0',
-            description='Joystick device pathy'
+            description='Joystick device path'
         ),
         DeclareLaunchArgument(
             'deadzone',
             default_value='0.05',
             description='Joystick deadzone'
-        ),
-        DeclareLaunchArgument(
-            'cmd_vel_topic',
-            default_value='/joy_raw_cmd_vel',
-            description='Output velocity command topic'
         ),
         DeclareLaunchArgument(
             'autorepeat_rate',
@@ -75,6 +58,5 @@ def generate_launch_description():
                         'joystick will only send on state change'
         ),
         joy_node,
-        teleop_node,
         joy_interpreter,
     ])
