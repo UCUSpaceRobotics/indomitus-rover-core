425ce6f1c4a47c2a796fd8b7a2703a81a016e8b2
yuriifito <y.y.fityo@gmail.com>
2026-07-09 23:43:14 +0300

    tab=4, not 2

--- src/rover_sim/include/rover_sim/rocker_diff_controller.hpp
+++ src/rover_sim/include/rover_sim/rocker_diff_controller.hpp
@@ -7,16 +7,16 @@ namespace rover_sim {
 class RockerDiffController : public controller_interface::ControllerInterface {
 public:
   controller_interface::InterfaceConfiguration command_interface_configuration() const override;
-  controller_interface::InterfaceConfiguration state_interface_configuration() const override;
+    controller_interface::InterfaceConfiguration state_interface_configuration() const override;
 
-  controller_interface::CallbackReturn on_init() override;
-  controller_interface::CallbackReturn on_configure(const rclcpp_lifecycle::State &) override;
+    controller_interface::CallbackReturn on_init() override;
+    controller_interface::CallbackReturn on_configure(const rclcpp_lifecycle::State &) override;
 
-  controller_interface::return_type update(
-    const rclcpp::Time & time, const rclcpp::Duration & period) override;
+    controller_interface::return_type update(
+        const rclcpp::Time & time, const rclcpp::Duration & period) override;
 
 private:
-  double k_, d_, effort_limit_;
+    double k_, d_, effort_limit_;
 };
 
 }
