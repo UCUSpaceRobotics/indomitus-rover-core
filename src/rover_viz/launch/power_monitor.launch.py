91bbcaa4e94ea0b0898921c756c4be38f9357a2b
yuriifito <y.y.fityo@gmail.com>
2026-07-11 01:48:24 +0300

    feature: add plotjuggler for current and voltage visualization

---
+++ src/rover_viz/launch/power_monitor.launch.py
@@ -0,0 +1,18 @@
+import os
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+
+def generate_launch_description():
+    layout_path = os.path.join(get_package_share_directory('rover_viz'), 'plotjuggler', 'power_monitor.xml')
+
+    plotjuggler = Node(
+        package='plotjuggler',
+        executable='plotjuggler',
+        name='power_monitor',
+        arguments=['-l', layout_path]
+    )
+    
+    return LaunchDescription([
+        plotjuggler
+    ])
