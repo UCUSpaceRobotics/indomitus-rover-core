0bbf29bcbec367141c2f1e55683ee06746125629
milkaside <milkaside.uni@gmail.com>
2026-07-14 16:41:58 +0300

    feat(arm): add primitive-geometry arm variant and use it in Gazebo sim

--- src/arm/arm_sim/launch/arm_gazebo.launch.py
+++ src/arm/arm_sim/launch/arm_gazebo.launch.py
@@ -23,7 +23,7 @@ def generate_launch_description() -> LaunchDescription:
     arm_sim_dir = get_package_share_directory("arm_sim")
     ros_gz_sim_dir = get_package_share_directory("ros_gz_sim")
 
-    xacro_file = os.path.join(arm_description_dir, "urdf", "arm_standalone.urdf.xacro")
+    xacro_file = os.path.join(arm_description_dir, "urdf", "arm_standalone_simple.urdf.xacro")
     world_file = os.path.join(arm_sim_dir, "worlds", "empty.sdf")
     bridge_config = os.path.join(arm_sim_dir, "config", "gz_bridge.yaml")
 
