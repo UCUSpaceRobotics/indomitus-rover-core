f6f249f57b21b62f895a924c28c299027ffd99f2
BohdanZasymovych <bohdan.zasymovych@gmail.com>
2026-07-21 15:53:57 +0300

    fix: launch arguments added to the zed2i.launch.py to fix the issue with parameter override by the launchfile from zed_wraper

--- src/rover_sensors/launch/zed2i.launch.py
+++ src/rover_sensors/launch/zed2i.launch.py
@@ -1,9 +1,10 @@
+import os
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, GroupAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.substitutions import FindPackageShare
 from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
-from launch_ros.actions import SetRemap # <-- Import SetRemap
+from launch_ros.actions import SetRemap
 
 DEFAULT_ZED2I_CONFIG_NAME = "zed2i.yaml"
 CAMERA_MODEL = "zed2i"
@@ -20,6 +21,27 @@ def generate_launch_description():
         name="config_path",
         default_value=config_path,
         description="Path to the config for the ZED 2i stereo camera"
+    )
+
+    publish_tf_arg = DeclareLaunchArgument(
+        'publish_tf',
+        default_value='false',
+        description='Enable publication of the `odom -> camera_link` TF.',
+        choices=['true', 'false']
+    )
+    
+    publish_map_tf_arg = DeclareLaunchArgument(
+        'publish_map_tf',
+        default_value='false',
+        description='Enable publication of the `map -> odom` TF. Note: Ignored if `publish_tf` is False.',
+        choices=['true', 'false']
+    )
+    
+    publish_urdf_arg = DeclareLaunchArgument(
+        'publish_urdf',
+        default_value='false',
+        description='Enable URDF processing and starts Robot State Published to propagate static TF.',
+        choices=['true', 'false']
     )
 
     launch_file_with_remappings = GroupAction(
@@ -46,6 +68,9 @@ def generate_launch_description():
                     "camera_model": CAMERA_MODEL,
                     "ros_params_override_path": LaunchConfiguration("config_path"),
                     "camera_name": CAMERA_MODEL,
+                    "publish_tf": LaunchConfiguration("publish_tf"),
+                    "publish_map_tf": LaunchConfiguration("publish_map_tf"),
+                    "publish_urdf": LaunchConfiguration("publish_urdf"),
                 }.items()
             )
         ]
@@ -53,5 +78,8 @@ def generate_launch_description():
 
     return LaunchDescription([
         config_path_argument,
+        publish_tf_arg,
+        publish_map_tf_arg,
+        publish_urdf_arg,
         launch_file_with_remappings,
     ])
